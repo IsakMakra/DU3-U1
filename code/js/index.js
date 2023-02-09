dc9b9b2bdce8990cffd3498f8750ee1f862208fd
IsakMakra <isak.makra01@gmail.com>
2023-02-09 11:47:18 +0100

    added some notes

--- code/js/index.js
+++ code/js/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 let account_prefix = "https://teaching.maumt.se/apis/access/";
+// JSON, 
 
 let dog_prefix_start = "https://dog.ceo/api/breed/";
 let dog_prefix_end = "/images/random";
+// JSON, the key "message" of the resource is a link to the picture.
