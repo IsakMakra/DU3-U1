d62c0cf6462b8280003a1b6defd834d1dd617c3d
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2023-02-10 11:41:08 +0100

    created some prefixes

--- code/js/index.js
+++ code/js/index.js
@@ -1,3 +1,6 @@
 "use strict";
 
-let prefix = "https://teaching.maumt.se/apis/access/";
+let account_prefix = "https://teaching.maumt.se/apis/access/";
+
+let dog_prefix_start = "https://dog.ceo/api/breed/";
+let dog_prefix_end = "/images/random";
