45d03ee7c0f4db442b94023556984aec4b6aa59f
IsakMakra <isak.makra01@gmail.com>
2023-02-12 12:14:26 +0100

    wrote a function "fetch_handler"

--- code/js/index.js
+++ code/js/index.js
@@ -6,3 +6,6 @@ let account_prefix = "https://teaching.maumt.se/apis/access/";
 let dog_prefix_start = "https://dog.ceo/api/breed/";
 let dog_prefix_end = "/images/random";
 // JSON, the key "message" of the resource is a link to the picture.
+
+//Call functions...
+
--- code/js/server_connection.js
+++ code/js/server_connection.js
@@ -1,1 +1,7 @@
 "use strict";
+
+async function fetch_handler (URL) {
+    let response = await fetch(URL);
+    let resource = await response.json();
+    return resource;
+}
