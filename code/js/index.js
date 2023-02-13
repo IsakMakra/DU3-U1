07d15c49bdad41fc3334615eb921c8a8071772d6
IsakMakra <isak.makra01@gmail.com>
2023-02-13 13:36:12 +0100

    Fixed script orders in html and wrote some functions in index.js

--- code/js/index.js
+++ code/js/index.js
@@ -2,10 +2,15 @@
 
 let account_prefix = "https://teaching.maumt.se/apis/access/";
 // JSON, 
+//POST - 
+//GET - 
 
-let dog_prefix_start = "https://dog.ceo/api/breed/";
-let dog_prefix_end = "/images/random";
-// JSON, the key "message" of the resource is a link to the picture.
+function get_pic_prefix(breed_url) {
+    let URL = `https://dog.ceo/api/breed/${breed_url}/images/random`;
+    return URL;
+}
+//console.log(fetch_handler(get_breed_prefix(ALL_BREEDS[0].url)));
+// argument must be ALL_BREEDS[?].url
+// do a fetch with the new URL (JSON), the key "message" of the resource is a link to the picture.
 
-//Call functions...
 
